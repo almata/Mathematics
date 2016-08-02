6be4aeee20c08989eeb96726e286d4986b53bd7e
Albert Mata <hello@albertmata.net>
2016-08-02 16:45:32 +0200

    Initial Commit

---
+++ Mathematics/Mathematics.h
@@ -0,0 +1,19 @@
+//
+//  Mathematics.h
+//  Mathematics
+//
+//  Created by Albert Mata Guerra on 02/08/16.
+//  Copyright © 2016 Albert Mata Guerra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mathematics.
+FOUNDATION_EXPORT double MathematicsVersionNumber;
+
+//! Project version string for Mathematics.
+FOUNDATION_EXPORT const unsigned char MathematicsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mathematics/PublicHeader.h>
+
+
